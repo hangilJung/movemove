1ecc71fa6f75304612f5c16030e785424a9160e6
hangilJung <ever_g@naver.com>
2021-11-16 15:52:38 +0900

    App.js수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Suspense } from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
@@ -6,9 +5,10 @@ import PublicRoute from "./lib/PublicRoute";
 import PrivateRoute from "./lib/PrivateRoute";
 import Login from "./components/views/LoginPage/Login";
 import Navbar from "./components/views/NavBar/Navbar";
-import DateSearchPage from "./components/views/DateSearchPage/DateSearchPage";
 import LandingPage from "./components/views/LandingPage/LandingPage";
+import DateSearchPage from "./components/views/DateSearchPage/DateSearchPage";
 import StatisticsPage from "./components/views/StatisticsPage/StatisticsPage";
+import Header from "./components/views/Header/Header";
 import Footer from "./components/views/Footer/Footer";
 import KioskPage from "./components/views/KioskPage/KioskPage";
 import NotFound from "./components/views/NotFoundPage";
@@ -18,29 +18,6 @@ import { Spin } from "antd";
 import SettingPage from "./components/views/SetPage/SettingPage";
 import SetRisk from "./components/views/SetRiskPage/SetRiskPage";
 import SetPassword from "./components/views/SetPassword/SetPassword";
-=======
-import React, { Suspense } from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import './App.css';
-import PublicRoute from './lib/PublicRoute';
-import PrivateRoute from './lib/PrivateRoute';
-import Login from './components/views/LoginPage/Login';
-import Navbar from './components/views/NavBar/Navbar';
-import LandingPage from './components/views/LandingPage/LandingPage';
-import DateSearchPage from './components/views/DateSearchPage/DateSearchPage';
-import StatisticsPage from './components/views/StatisticsPage/StatisticsPage';
-import SettingPage from './components/views/SettingPage/SettingPage';
-import Header from './components/views/Header/Header';
-import Footer from './components/views/Footer/Footer';
-import KioskPage from './components/views/KioskPage/KioskPage';
-import NotFound from './components/views/NotFoundPage';
-import TestPage from './components/views/TestPage/Test';
-import TestPage2 from './components/views/Test2Page/Test2';
-import { Spin } from 'antd';
-import SetPage from './components/views/SetPage/SetPage';
-import SetRisk from './components/views/SetRiskPage/SetRiskPage';
-import SetPassword from './components/views/SetPassword/SetPassword';
->>>>>>> ca3b56b6e98c728b93cba215907d8f5cd1f10fae
 
 function App() {
   return (
@@ -58,7 +35,6 @@ function App() {
            PublicRoute => 아무나 출입 가능
            PublicRoute restricted => 로그인한 유저 출입 불가
        */}
-<<<<<<< HEAD
         <div>
           <Switch>
             <PrivateRoute exact path="/" component={LandingPage} />
@@ -75,23 +51,6 @@ function App() {
             <PublicRoute exact path="/setPassword" component={SetPassword} />
           </Switch>
         </div>
-=======
-        <Switch>
-          <PrivateRoute exact path="/" component={LandingPage} />
-          <PublicRoute restricted exact path="/login" component={Login} />
-          <PrivateRoute exact path="/landing" component={LandingPage} />
-          <PublicRoute exact path="/search" component={DateSearchPage} />
-          <PrivateRoute exact path="/statistics" component={StatisticsPage} />
-          <PrivateRoute exact path="/setting" component={SettingPage} />
-          <PublicRoute exact path="/kiosk" component={KioskPage} />
-          <PublicRoute exact path="/test" component={TestPage} />
-          <PublicRoute exact path="/test2" component={TestPage2} />
-          <PublicRoute exact path="/notfound" component={NotFound} />
-          <PublicRoute exact path="/set" component={SetPage} />
-          <PublicRoute exact path="/setRisk" component={SetRisk} />
-          <PublicRoute exact path="/setPassword" component={SetPassword} />
-        </Switch>
->>>>>>> ca3b56b6e98c728b93cba215907d8f5cd1f10fae
         <Footer />
       </BrowserRouter>
     </Suspense>
